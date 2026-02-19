a24ed9ab758dcfbd549db3edf56259dc22c19f95
iantato <margiehouses@gmail.com>
2026-02-19 18:21:25 +0800

    Added `DatabaseException`

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,3 +2,6 @@ class ScraperException(Exception):
     """Base custom exception for Scraper failures"""
     pass
 
+class DatabaseException(Exception):
+    """Base custom exception for Database failures"""
+    pass
